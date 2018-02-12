d21fb7e1a11400aa7dd7bf8018213550a16cf28d
Marcin Baran <mrevib@gmail.com>
2018-02-12 19:01:18 +0100

    Zmiana wystwietlania strony show

--- pages/show.php
+++ pages/show.php
@@ -1,17 +1,27 @@
 <?php
 	$row = $function->get_info_about($_GET['show']);
-	echo '<h2>Informacje o instrumencie: '.$_GET['show'].'</h2>';
+	echo '<h2>'.$_GET['show'].'</h2>';
 	$rodzaj = $row[0]->values()[1]->value('nazwa');
 	$pochodzenie = $function->get_origin($rodzaj);
 ?>
 <div class="row">
+	<div class="col-md-4">
+		<?php
+			if(file_exists('./img/instruments/'.$_GET['show'].'.png')){
+				echo '<center><img src="./img/instruments/'.$_GET['show'].'.png" class="img-responsive img-thumbnail" ></center>';
+			}else{
+				echo '<center><img src="/img/instruments/brak_zdjecia.png" class="img-responsive img-thumbnail"></center>';
+			}
+		?>
+		<h5>Nazwa: <?php echo $_GET['show']; ?></h5>
+		<h5>Rodzaj: <?php echo $row[0]->values()[1]->value('nazwa'); ?></h5>
+		<h5>Rodzina: <?php echo $row[0]->values()[2]->value('nazwa'); ?></h5>
+		<h5>Podgrupa: <?php echo $row[0]->values()[3]->value('nazwa'); ?></h5>
+		<h5>Grupa: <?php echo $row[0]->values()[4]->value('nazwa'); ?></h5>
+		<br>
+		<a href="javascript:history.go(-1);"><button class="btn btn-default">Wróć</button></a>
+	</div>
 	<div class="col-md-8">
-		<h4>Nazwa: <?php echo $_GET['show']; ?></h4>
-		<h4>Rodzaj: <?php echo $row[0]->values()[1]->value('nazwa'); ?></h4>
-		<h4>Rodzina: <?php echo $row[0]->values()[2]->value('nazwa'); ?></h4>
-		<h4>Podgrupa: <?php echo $row[0]->values()[3]->value('nazwa'); ?></h4>
-		<h4>Grupa: <?php echo $row[0]->values()[4]->value('nazwa'); ?></h4>
-		<hr></hr>
 		<h3>Wywodzenie się instrumentu:</h3>
 		<div class="table-responsive">
 			<table class="table">
@@ -22,22 +32,36 @@
 					</tr>
 				</thead>
 					<tr>
-						<td><?php echo $pochodzenie[0]->values()[1]->value('nazwa'); ?></td>
-						<td><?php echo $pochodzenie[0]->values()[2]->value('nazwa'); ?></td>
+						<td>
+							<?php
+							if(isset($pochodzenie[0])){
+								echo $pochodzenie[0]->values()[1]->value('nazwa');
+							}else{
+								echo '-';
+							} 
+								 
+							?>
+						</td>
+						<td>
+							<?php
+							if(isset($pochodzenie[0])){
+								echo $pochodzenie[0]->values()[2]->value('nazwa');
+							}else{
+								echo '-';
+							} 
+								 
+							?>
 					</tr>
 				<tbody>
 				</tbody>
 			</table>
 		</div>
-	</div>
-	<div class="col-md-4">
-		<?php
-			echo '<h4 style="text-align: center;">'.$_GET['show'].'</h4>';
-			if(file_exists('./img/instruments/'.$_GET['show'].'.png')){
-				echo '<center><img src="./img/instruments/'.$_GET['show'].'.png" class="img-responsive img-thumbnail" ></center>';
-			}else{
-				echo '<center><img src="/img/instruments/brak_zdjecia.png" class="img-responsive img-thumbnail"></center>';
-			}
-		?>
+		<hr></hr>
+		<h3>Opis</h3>
+		<hr></hr>
+		<h3>Kształt</h3>
+		<hr></hr>
+		<h3>Skala</h3>
 	</div>
 </div>
+<br><br><br><br><br><br>
